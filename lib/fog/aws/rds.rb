942b078b300fc37b703d174d58cb7e2dfdc5edb3
Josh Lane & Michelle Noorali <jlane@engineyard.com>
2014-12-17 15:59:45 -0800

    let me have region on rds

--- lib/fog/aws/rds.rb
+++ lib/fog/aws/rds.rb
@@ -111,6 +111,8 @@ module Fog
           @data = nil
         end
 
+        attr_accessor :region
+
         def initialize(options={})
           @use_iam_profile = options[:use_iam_profile]
           @region = options[:region] || 'us-east-1'
